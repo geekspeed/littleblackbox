80800c89b7b7642496f6469430ec24c76308e9b5
heffnercj <heffnercj@787c237b-256d-019b-ad23-55655ec97743>
2010-12-23 03:26:26 +0000

    Updated database with more Netgear SSL VPN certs

--- src/main.c
+++ src/main.c
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
 	/* Make sure we have a fingerprint */
 	if(!certinfo.fingerprint)
 	{
-		fprintf(stderr, "ERROR: No suitable certificate fingerprint provided!\n");
+		fprintf(stderr, "No suitable certificate fingerprint provided!\n");
 		goto end;
 	}
 
